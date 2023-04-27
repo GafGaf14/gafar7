5b126c49b2d280d207f3687561de2c1e680380ba
GafGaf14 <131952778+GafGaf14@users.noreply.github.com>
2023-04-27 10:45:37 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let list = document.querySelectorAll(".list li");
+let box = document.querySelectorAll(".box");
+
+list.forEach((el)=>{
+    el.addEventListener("click" , (e)=>{
+        list.forEach((el1)=>{
+            el1.style.color = "#000";
+        })
+        e.target.style.color = "#464553"
+        box.forEach((el2)=>{
+            el2.style.display = "none";
+        })
+        document.querySelectorAll(e.target.dataset.color).forEach((el3)=>{
+            el3.style.display = "flex";
+        })
+    })
+})
+document.querySelector('.open').addEventListener('click', () =>{
+    document.querySelector('.burger').classList.toggle('active')
+})
